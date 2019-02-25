5215d5ad7cc36671a700074f89e0c856c0590338
Luis <lpompa@hotmail.com>
2019-02-25 10:48:16 -0700

    Actualizar tiempo

--- Reproductor/MainWindow.xaml.cs
+++ Reproductor/MainWindow.xaml.cs
@@ -18,6 +18,8 @@ using Microsoft.Win32;
 using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
 
+using System.Windows.Threading;
+
 namespace Reproductor
 {
     /// <summary>
@@ -29,10 +31,29 @@ namespace Reproductor
         //Nuestra comunicacion con la tarjeta de sonido
         WaveOutEvent output;
 
+        DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
             LlenarComboSalida();
+
+            //Inicializar timer
+            //Establecer tiempo entre ejecuciones
+            //Establecer lo que se va a ejecutar
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                lblTiempoActual.Text =
+                    reader.CurrentTime.ToString().Substring(0, 8);
+            }
         }
 
         private void LlenarComboSalida()
@@ -90,6 +111,10 @@ namespace Reproductor
 
                 lblTiempoTotal.Text =
                     reader.TotalTime.ToString().Substring(0, 8);
+                lblTiempoActual.Text =
+                    reader.CurrentTime.ToString().Substring(0, 8);
+
+                timer.Start();
         
             }
 
@@ -99,6 +124,7 @@ namespace Reproductor
         {
             reader.Dispose();
             output.Dispose();
+            timer.Stop();
         }
 
         private void btnPausa_Click(object sender, RoutedEventArgs e)
